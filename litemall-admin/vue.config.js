5270875dbeff0dbd9f3cf0035a04a36d9a911a33
yujiufeng <yujiufeng87@163.com>
2020-08-16 01:38:42 +0800

    修改了title

--- litemall-admin/vue.config.js
+++ litemall-admin/vue.config.js
@@ -5,7 +5,7 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
-const name = 'litemall' // page title
+const name = '照片商城管理' // page title
 
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
